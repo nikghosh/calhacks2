e6309c2e5f5b2cdc3cf7bcc943848d517235bec7
Kazu Kogachi <kkogachi@berkeley.edu>
2015-10-10 20:53:38 -0700

    name changes and some comments

--- get_addresses.py
+++ get_addresses.py
@@ -1,33 +1,34 @@
 import urllib.request
 import json
-import pprint
+
+"""
+Gets the address from CapitalOne Hackathon API accounts
+Kazu Kogachi, Cal Hacks 2.0
+"""
 
 customerID = '560f0205f8d8770df0ef99e7'
 apiKey = 'd3a81dbab024b45a8aa3857d732d67ee'
 
-url = 'http://api.reimaginebanking.com/enterprise/accounts?key={}'.format(apiKey)
-#url = 'http://api.reimaginebanking.com/enterprise/customers/{}?key={}'.format(customerID,apiKey)
 
+#Gets list of accounts from API
+url = 'http://api.reimaginebanking.com/enterprise/accounts?key={}'.format(apiKey)
 test = urllib.request.Request(url)
 response = urllib.request.urlopen(test)
-raw = response.read()
+raw = response.read() #raw is in bytes lol
 lst = json.loads(raw.decode('utf-8'))['results']
 #print(str(lst))
 
+memo = [] #removes duplicate addresses
+f = open('addresses.txt','w')
 
-memo = []
-f = open('test','w')
-#f.write(str(lst))
-
+#Writes each address to f
 for obj in lst:
     new_url = 'http://api.reimaginebanking.com/enterprise/customers/{}?key={}'.format(obj['customer_id'],apiKey)
     temp_req = urllib.request.Request(new_url)
     temp_resp = urllib.request.urlopen(temp_req)
     temp_raw = temp_resp.read()
     data = json.loads(temp_raw.decode('utf-8'))
-    print('writing data')
     if data['address'] not in memo:
-        print('data unique')
         memo.append(data['address'])
         f.write(str(data['address'])+'\n')
 
---
+++ loc_from_address.py
@@ -0,0 +1,70 @@
+import urllib.request
+import json
+
+
+"""
+Converts a series of addresses into latitude longitude coordinates
+Uses the HERE Geocoder API
+Kazu Kogachi, Cal Hacks 2.0
+"""
+
+
+appId = '5wWMgIqkEQkCbqjJes4x'
+appCode = '27suaUwub3nbfaBTDbEDEw'
+
+url = 'https://geocoder.cit.api.here.com/6.2/geocode.json?app_id={}&app_code={}'.format(appId, appCode)
+
+#print(str(lst))
+
+try:
+    f = open('addresses.txt','r')
+except IOError:
+    print("Cannot read file addresses.txt")
+except Exception as e:
+    print(str(e))
+    f.close()
+else:
+    line = f.readline()
+    writer = open('ohio_loc.txt','w')
+    while line != '':
+        d = 'dict({})'.format(line)
+        d = eval(d)
+        
+        if d['state'] == 'Ohio': #condition
+            req_url = url + "&gen=9&housenumber={}&street={}&city={}".format(d['street_number'],
+                                                                             d['street_name'],
+                                                                             d['city'])
+            req_url = req_url.replace(' ','%20')
+            req = urllib.request.Request(req_url)
+            response = urllib.request.urlopen(req)
+            raw = response.read()
+            raw = raw.decode('utf-8')
+            d_raw = eval('dict({})'.format(raw))
+
+            """debugging code
+            print('1'+ str(d_raw['Response']))
+            print('2'+ str(d_raw['Response']['View']))
+            print('3'+ str(d_raw['Response']['View'][0]))
+            print('4'+ str(d_raw['Response']['View'][0]['Result']))
+            print('5'+ str(d_raw['Response']['View'][0]['Result'][0]))
+            print('6'+ str(d_raw['Response']['View'][0]['Result'][0]['Location']))
+            print('7'+ str(d_raw['Response']['View'][0]['Result'][0]['Location']['DisplayPosition']))
+            """
+            
+            to_write = ''
+            try:
+                temp = d_raw['Response']['View'][0]['Result'][0]['Location']['DisplayPosition']
+                to_write = str(temp['Longitude']) + ' ' + str(temp['Latitude']) + '\n'
+            except Exception as e:
+                print('lol why is something null\n' + str(e))
+                
+            print("WRITING " + repr(to_write))
+            writer.write(to_write)
+                
+        line = f.readline()
+        print('NEXT LINE READ: ' + str(line))
+        
+    writer.close()
+    f.close()
+
+print("here api - finished")
